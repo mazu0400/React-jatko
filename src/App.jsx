1ab790fc9d43ddbc57d88b2958676e2058f010a0
mazu0400 <matias.tolvanen13@gmail.com>
2025-12-04 16:15:09 +0200

    fix hrapp step6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Person.css";
 export default function Stats({ employees }) {
   const totalSalary = employees.reduce(
-    (sum, emp) => sum + Number(emp.salary),
+    (sum, emp) => sum + Number(emp.salary || 0),
     0
   );
   const avgSalary =
